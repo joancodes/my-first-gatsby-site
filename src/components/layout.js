508990114e097844a0c34259296bc1c3583f3de3
Joan Nyambura <njugunajoan35@gmail.com>
2023-02-13 12:22:37 +0300

    Part-4: Query for Data with GraphQL

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,27 @@
 import * as React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
 import { Link } from 'gatsby'
 import { container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText } from './layout.module.css'
+  navLinkText,
+  siteTitle } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <div className={container}>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
@@ -17,7 +30,14 @@ const Layout = ({ pageTitle, children }) => {
             </Link>
           </li>
           <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>About</Link>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
           </li>
         </ul>
       </nav>
